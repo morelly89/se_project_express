4cec5c95b429cdb51cb0e72b02377a733cad2b03
morelly89 <morelly89@yahoo.com>
2025-09-02 19:49:53 -0400

    app.js catch block modified

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const { PORT = 3001 } = process.env;
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => {})
-  .catch((err) => console.error(err));
+  .catch((err) => {
+    throw new Error(`MongoDB connection failed: ${err.message}`);
+  });
 app.use(express.json());
 app.use((req, res, next) => {
   req.user = {
